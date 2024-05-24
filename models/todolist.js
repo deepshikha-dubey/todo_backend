5c525c269ad1a903a401423876ffa1b77558a34e
deepshikha-dubey <dubeydeepshikha1707@gmail.com>
2024-05-24 15:29:45 +0530

    Todo_List

---
+++ models/todolist.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+
+const todoSchema = new mongoose.Schema({
+    task: {
+        type:String,
+        required:true,
+    },
+    status: {
+        type: String,
+        required:true,
+    },
+    deadline:{
+        type: Date,
+    },
+});
+
+const todoList = mongoose.model("todo", todoSchema);
+
+export default todoList;
